e9cfe29cd8bedee0adfe83331abcfdb2a1d86fab
Ahmed Hussain <01.ahmed.hussain@gmail.com>
2022-02-20 13:48:51 +0530

    commit

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -6,34 +6,58 @@ function Products(props) {
 
   return (
     <div>
-        <div className="cardRow">
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="card card-body">
-            <h4 className="productHeading">{props.children}</h4>
-            <div className="imgRow">
-              <div className="imgCol">
-                <img src={props.productObj.productImage1} className="itemImg" alt="..." />
-                <h4 className="productname">{props.productObj.productTitle1}</h4>
+      <div className="cardRow">
+        <div className="row">
+          <div className="col-sm-4">
+            <div className="card card-body">
+              <h4 className="productHeading">{props.children}</h4>
+              <div className="imgRow">
+                <div className="imgCol">
+                  <img
+                    src={props.productObj.productImage1}
+                    className="itemImg"
+                    alt="..."
+                  />
+                  <h4 className="productname">
+                    {props.productObj.productTitle1}
+                  </h4>
+                </div>
+                <div className="imgCol">
+                  <img
+                    src={props.productObj.productImage2}
+                    className="itemImg"
+                    alt="..."
+                  />
+                  <h4 className="productname">
+                    {props.productObj.productTitle2}
+                  </h4>
+                </div>
               </div>
-              <div className="imgCol">
-                <img src={props.productObj.productImage2} className="itemImg" alt="..." />
-                <h4 className="productname">{props.productObj.productTitle2}</h4>
-              </div>
-            </div>
-            <div className="imgRow">
-            <div className="imgCol">
-                <img src={props.productObj.productImage3} className="itemImg" alt="..." />
-                <h4 className="productname">{props.productObj.productTitle3}</h4>
-              </div>
-              <div className="imgCol">
-                <img src={props.productObj.productImage4} className="itemImg" alt="..." />
-                <h4 className="productname">{props.productObj.productTitle4}</h4>
+              <div className="imgRow">
+                <div className="imgCol">
+                  <img
+                    src={props.productObj.productImage3}
+                    className="itemImg"
+                    alt="..."
+                  />
+                  <h4 className="productname">
+                    {props.productObj.productTitle3}
+                  </h4>
+                </div>
+                <div className="imgCol">
+                  <img
+                    src={props.productObj.productImage4}
+                    className="itemImg"
+                    alt="..."
+                  />
+                  <h4 className="productname">
+                    {props.productObj.productTitle4}
+                  </h4>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
